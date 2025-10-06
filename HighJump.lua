6638a981d40932a308b615bdd2f3ecb2339308fc
Menwey <inova6k@gmail.com>
2025-10-06 22:18:05 +0300

    Update HighJump.lua

--- HighJump.lua
+++ HighJump.lua
@@ -1,11 +1,16 @@
-function HighJump(power)
-    power = power or 150
+-- HighJump отдельно
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+function SetHighJump(power)
+    power = power or 100
     local character = LocalPlayer.Character
     if character then
         local humanoid = character:FindFirstChild("Humanoid")
         if humanoid then
             humanoid.JumpPower = power
-            print("✅ HighJump: " .. power)
         end
     end
 end
+
+SetHighJump(100)
