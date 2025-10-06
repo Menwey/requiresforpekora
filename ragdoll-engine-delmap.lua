6b2b27a6259801fb30ba01032ffaa952f23a64cc
Menwey <inova6k@gmail.com>
2025-10-06 21:12:39 +0300

    Create ragdoll-engine-delmap.lua

---
+++ ragdoll-engine-delmap.lua
@@ -0,0 +1,10 @@
+local tool
+for i,v in pairs (game.Players:GetPlayers()) do
+ tool = v.Backpack:FindFirstChild'OddPotion' or v.Character:FindFirstChild'OddPotion'
+ if tool then break end
+end
+ 
+for i,v in pairs(workspace.NewerMap:GetDescendants()) do
+ tool.TransEvent:FireServer(v,1)
+end
+-- made by pozzibros and a4
